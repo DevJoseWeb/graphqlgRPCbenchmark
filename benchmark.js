dd659fc166954d2cff32df98f3bc885b018aac5d
Rodrigo Quezada <roderik.ikana@gmail.com>
2017-12-24 18:11:37 -0600

    feat: add final benchmark

--- benchmark.js
+++ benchmark.js
@@ -1,45 +1,78 @@
 const Benchmark = require('benchmark')
 const axios = require('axios')
-const suite = new Benchmark.Suite
+const jsonfile = require('jsonfile')
+const path = require('path')
+const Promise = require('bluebird')
  
-// add tests 
-suite.add('grpc',{
-  defer: true,
-  fn: (df) => {
-    axios.post('http://localhost:4000', {
-      query: "{\n  grpc(size: 10, parts: 20) {\n    name\n    phone\n    id\n    profilePic\n    birthday\n    balance\n  }\n}"
-    }).then(() => {
-      df.resolve()
+const test = (size, parts) => () => new Promise((resolve, reject) => { 
+  const suite = new Benchmark.Suite
+  const res = {}
+  suite.add('grpc',{
+    defer: true,
+    minSamples: 20,
+    fn: (df) => {
+      axios.post('http://localhost:4000', {
+        query: `{\n  grpc(size: ${size}, parts: ${parts}) {\n    name\n    phone\n    id\n    profilePic\n    birthday\n    balance\n  }\n}`
+      }).then(() => {
+        df.resolve()
+      })
+    }
+  })
+  .add('http',{
+    defer: true,
+    minSamples: 20,
+    fn: (df) => {
+      axios.post('http://localhost:4000', {
+        query: `{\n  http(size: ${size}, parts: ${parts}) {\n    name\n    phone\n    id\n    profilePic\n    birthday\n    balance\n  }\n}`        
+      }).then(() => {
+        df.resolve()
+      })
+    }
+  })
+  .add('httpc', {
+    defer: true,
+    minSamples: 20,
+    fn: (df) => {
+      axios.post('http://localhost:4000', {
+        query: `{\n  httpc(size: ${size}, parts: ${parts}) {\n    name\n    phone\n    id\n    profilePic\n    birthday\n    balance\n  }\n}`        
+      }).then(() => {
+        df.resolve()
+      })
+    }
+  })
+  // add listeners 
+  .on('cycle', function(event) {
+    res[event.target.name] = {
+      sample: event.target.stats.sample.length,
+      rme: event.target.stats.rme,
+      hz: event.target.hz
+    }
+  })
+
+  .on('complete', function() {
+    resolve({
+      fast: this.filter('fastest').map('name').join(','),
+      res
     })
-  }
+  })
+  // run async 
+  .run({ 'async': true })
 })
-.add('http',{
-  defer: true,
-  fn: (df) => {
-    axios.post('http://localhost:4000', {
-      query: "{\n  http(size: 10, parts: 20) {\n    name\n    phone\n    id\n    profilePic\n    birthday\n    balance\n  }\n}"
-    }).then(() => {
-      df.resolve()
-    })
-  }
-})
-.add('httpc', {
-  defer: true,
-  fn: (df) => {
-    axios.post('http://localhost:4000', {
-      query: "{\n  httpc(size: 10, parts: 20) {\n    name\n    phone\n    id\n    profilePic\n    birthday\n    balance\n  }\n}"
-    }).then(() => {
-      df.resolve()
-    })
-  }
-})
-// add listeners 
-.on('cycle', function(event) {
-  console.log(String(event.target));
-})
-.on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
-})
-// run async 
-.run({ 'async': true })
- 
+
+const funcs = Promise.resolve([
+  [1, 1],
+  [1, 10],
+  [1, 100],
+  [1, 1000],
+  [1, 1],
+  [10, 1],
+  [100, 1],
+  [1000, 1],
+].map(([a, b]) => test(a, b)))
+
+funcs
+  .mapSeries((f) => f())  // logs: 500, 100, 400, 200
+  .then((res) => {
+    console.log(res)
+    jsonfile.writeFileSync(path.join(__dirname, 'results.json'), res)
+  })
